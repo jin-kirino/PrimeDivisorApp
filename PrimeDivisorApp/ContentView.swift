6989cb0ae2fb7ea51776697867edd7d70a91a4a5
jin-kirino <jinkirino@gmail.com>
2023-03-31 06:53:41 +0900

    ボタンタップで素数と約数を抽出し、リストに表示させる

--- PrimeDivisorApp/ContentView.swift
+++ PrimeDivisorApp/ContentView.swift
@@ -19,16 +19,27 @@ struct ContentView: View {
                 .padding()
             HStack {
                 Button {
-                    print("素数ボタンがタップされた")
+                    if let castedNumber = canVastToIntType(inputText: inputText) {
+                        listNumbers = findPrimeNumbers(numberToCheck: castedNumber)
+                    } else {
+                        isShowingAlert.toggle()
+                    }
+                    inputText = ""
                     showCount = "素数"
+                    print("素数ボタンがタップされた")
                 } label: {
                     Text("素数チェック")
                 }
                 .buttonStyle(.borderedProminent)
                 Button {
-                    print("約数ボタンがタップされた")
+                    if let castedNumber = canVastToIntType(inputText: inputText) {
+                        listNumbers = findDivisors(numberToCheck: castedNumber)
+                    } else {
+                        isShowingAlert.toggle()
+                    }
+                    inputText = ""
                     showCount = "約数"
-
+                    print("約数ボタンがタップされた")
                 } label: {
                     Text("約数チェック")
                 }
@@ -42,6 +53,46 @@ struct ContentView: View {
         .alert("１以上の整数を入力してください", isPresented: $isShowingAlert) {
             Button("OK") { }
         }
+    }
+
+    func canVastToIntType(inputText: String) -> Int? {
+        if let intNumber = Int(inputText) {
+            if intNumber >= 1 {
+                return intNumber
+            }
+        }
+        return nil
+    }
+
+    func findDivisors(numberToCheck: Int) -> [Int] {
+        var divisors: [Int] = []
+        for number in 1 ... numberToCheck {
+            // 割り切れたら追加
+            if numberToCheck % number == 0 {
+                divisors.append(number)
+            }
+        }
+        return divisors
+    }
+
+    func findPrimeNumbers(numberToCheck: Int) -> [Int] {
+        var primeNumbers: [Int] = []
+        // 1~入力値
+        for number in 1 ... numberToCheck {
+            var count: Int = 0
+            // 1~自分自身で割る
+            for divisor in 1 ... number {
+                // 割り切れたら＋１
+                if number % divisor == 0 {
+                    count += 1
+                }
+            }
+            // 割り切れる回数が１と自分自身の２回
+            if count == 2 {
+                primeNumbers.append(number)
+            }
+        }
+        return primeNumbers
     }
 }
 
