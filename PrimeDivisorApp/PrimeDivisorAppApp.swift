54b5cda086d9071f7e7155078540dd6b33a02db7
jin-kirino <jinkirino@gmail.com>
2023-03-30 23:06:27 +0900

    first push

---
+++ PrimeDivisorApp/PrimeDivisorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeDivisorAppApp.swift
+//  PrimeDivisorApp
+//
+//  Created by 神　樹里乃 on 2023/03/30.
+//
+
+import SwiftUI
+
+@main
+struct PrimeDivisorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
