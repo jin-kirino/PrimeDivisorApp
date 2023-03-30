5dc2c0a17effae8769d28c194d83a35ff6d0d5f7
jin-kirino <jinkirino@gmail.com>
2023-03-31 06:45:00 +0900

    レイアウト作成

--- PrimeDivisorApp/ContentView.swift
+++ PrimeDivisorApp/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputText: String = ""
+    @State private var listNumbers: [Int] = []
+    @State private var showCount: String = ""
+    @State private var isShowingAlert: Bool = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("１以上の整数を入力してください", text: $inputText)
+                .padding()
+            HStack {
+                Button {
+                    print("素数ボタンがタップされた")
+                    showCount = "素数"
+                } label: {
+                    Text("素数チェック")
+                }
+                .buttonStyle(.borderedProminent)
+                Button {
+                    print("約数ボタンがタップされた")
+                    showCount = "約数"
+
+                } label: {
+                    Text("約数チェック")
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            Text("\(inputText) の \(showCount) は、\(listNumbers.count + 1) 個です")
+            List(listNumbers, id: \.self) { number in
+                Text("\(number)")
+            }
+        }// VStack
+        .alert("１以上の整数を入力してください", isPresented: $isShowingAlert) {
+            Button("OK") { }
         }
-        .padding()
     }
 }
 
